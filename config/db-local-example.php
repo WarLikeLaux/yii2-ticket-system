a78145abcb2e9ebabf903fb9f2933179a465987f
WarLikeLaux <warlikelaux080399@mail.ru>
2023-12-27 16:17:47 +0600

    Updated README.md

---
+++ config/db-local-example.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=yii2_ticket_system',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+];
